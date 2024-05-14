050fbdd7718ceaa3e0cee297efccb7d0fa1c59dc
BrandenConnors <brand3nconnors@gmail.com>
2024-05-13 17:53:01 -0700

    initial commit

---
+++ assets/php/send_email.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "realtybranden@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    if (mail($to, $subject, $body)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+} else {
+    echo "Method not allowed.";
+}
+?>
